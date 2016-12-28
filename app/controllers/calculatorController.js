8aa6477ed7a5986d48863df0044d62c07020d5b3
hellenfc <hellenfc93@gmail.com>
2016-12-28 03:34:51 -0600

    Calculator made, it adds and divides.

--- app/controllers/calculatorController.js
+++ app/controllers/calculatorController.js
@@ -1,3 +1,20 @@
 app.controller('calculatorCtrl', function(){
   var vm = this;
+  vm.firstNumber;
+  vm.secondNumber;
+  vm.operation;
+  vm.result;
+  vm.doOperation = function(){
+    if (vm.operation === "Add") {
+      vm.result = parseInt(vm.firstNumber) + parseInt(vm.secondNumber);
+    }else {
+      vm.result = parseInt(vm.firstNumber) / parseInt(vm.secondNumber);
+      if(String(vm.result) === "NaN"){
+        vm.result = "Indefinido";
+      } else if (String(vm.result) === "Infinity") {
+        vm.result = "Infinito";
+
+      }
+    }
+  }
 });
